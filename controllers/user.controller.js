e57f18f5bd16d8f0a3dbc2bccce6c9590ab15e0d
EloraBarua <elorabarua62@gmail.com>
2024-03-26 15:32:36 +0600

    get all students data API is created

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -53,6 +53,29 @@ class userControllers {
         res.status(500).json({ error: "Internal Server Error" });
     }
   };
+
+
+  // Controller: Load all students details
+  students_details = async(req, res) => {
+    const {page = 1, parPage = 0} = req.query;
+    const skip_elements = parseInt(page-1) * parseInt(parPage);
+    try {
+      const students = await User.find({ role: "student" })
+        .skip(skip_elements)
+        .limit(parseInt(parPage));
+
+      if(students.length){
+        let student_list = [];
+        students.map((student) => student_list.push({name: student.name, email: student.email, role: student.role}))
+        res.status(200).json({data: student_list, message: "Students Data has been loaded successfully" });
+      }
+      else{
+        res.status(400).json({ error: "No student is enlisted" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
 }
 
 module.exports = new userControllers();
--- routes/user.route.js
+++ routes/user.route.js
@@ -4,5 +4,6 @@ const router = require('express').Router();
 // Routes
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
+router.get("/students-details", userController.students_details);
 
 module.exports = router;
